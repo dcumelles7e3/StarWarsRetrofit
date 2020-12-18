6c457365dc29290e41808bf73bb658f28b827fee
dcumelles <didac.cumelles.7e3@itb.cat>
2020-12-18 22:51:27 +0100

    1.0

--- app/src/main/java/cat/itb/starwarsretrofit/activities/FilmActivity.java
+++ app/src/main/java/cat/itb/starwarsretrofit/activities/FilmActivity.java
@@ -5,9 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
 import cat.itb.starwarsretrofit.R;
 import cat.itb.starwarsretrofit.model.Data;
@@ -22,15 +26,17 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class FilmActivity extends AppCompatActivity {
-    private List<String> films;
+    private List<String> urlFilms;
     private TextView tvTitle;
     private TextView tvOpening;
+    private ListIterator<String> iterator;
+    private Button b_back;
+    private Button b_next;
+    private boolean iterForward;      //prevents from iterator skipping
 
     private Retrofit retrofit;
     private HttpLoggingInterceptor loggingInterceptor;
     private OkHttpClient.Builder httpClientBuilder;
-    private String next;
-    private String previous;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,16 +44,43 @@ public class FilmActivity extends AppCompatActivity {
         setContentView(R.layout.activity_film);
 
         final Bundle bundle = getIntent().getExtras();
-        films = bundle.getStringArrayList("filmlist");
-
+        urlFilms = bundle.getStringArrayList("filmlist");
         tvOpening = findViewById(R.id.tv_opening);
         tvTitle = findViewById(R.id.tv_title);
 
-        lanzarPeticion(films.get(0));
+        b_back = findViewById(R.id.b_filmback);
+        b_next = findViewById(R.id.b_filmnext);
+        iterator = urlFilms.listIterator();
+        lanzarPeticion(iterator.next());
+        iterForward=true;
 
+        b_back.setVisibility(View.INVISIBLE);
+        b_back.setOnClickListener(v -> {
+            if (iterForward) {
+                iterator.previous();
+                iterForward = false;
+            }
+            lanzarPeticion(iterator.previous());
+            if (!iterator.hasPrevious()) {
+                b_back.setVisibility(View.INVISIBLE);
+            }
+            b_next.setVisibility(View.VISIBLE);
+
+        });
+        b_next.setOnClickListener(v -> {
+            if (!iterForward) {
+                iterator.next();
+                iterForward = true;
+            }
+            lanzarPeticion(iterator.next());
+            if (!iterator.hasNext()) {
+                b_next.setVisibility(View.INVISIBLE);
+            }
+            b_back.setVisibility(View.VISIBLE);
+        });
     }
 
-    private void lanzarPeticion(String url) {
+    private synchronized void lanzarPeticion(String url) {
         loggingInterceptor = new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY);
         httpClientBuilder = new OkHttpClient.Builder().addInterceptor(loggingInterceptor);
 
@@ -57,25 +90,12 @@ public class FilmActivity extends AppCompatActivity {
                 .build();
 
         WebServiceClient client = retrofit.create(WebServiceClient.class);
-        Call<Film> call;
-            call = client.getFilm(url);
+        Call<Film> call = client.getFilm(url);
         call.enqueue(new Callback<Film>() {
             @Override
             public void onResponse(Call<Film> call, Response<Film> response) {
-                tvTitle.setText(response.body().getTitle());
                 tvOpening.setText(response.body().getOpening());
-//                next = response.body().getNext();
-//                if (next==null){
-//                    b_next.setVisibility(View.INVISIBLE);
-//                }else {
-//                    b_next.setVisibility(View.VISIBLE);
-//                }
-//                previous = response.body().getPrevious();
-//                if (previous==null){
-//                    b_back.setVisibility(View.INVISIBLE);
-//                }else {
-//                    b_back.setVisibility(View.VISIBLE);
-//                }
+                tvTitle.setText(response.body().getTitle());
             }
 
             @Override
